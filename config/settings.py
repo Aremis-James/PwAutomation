9d5f50f1d41a2fecd84b948272e2a30385460cf6
AremisJ <devpythonaj@gmail.com>
2024-02-27 00:03:51 -0500

    pydantic validation

--- config/settings.py
+++ config/settings.py
@@ -2,56 +2,72 @@ import os
 import toml
 import keyring as kr
 from functools import wraps
+from pydantic import BaseModel, EmailStr, HttpUrl
+from pydantic_settings import BaseSettings
+
 from dotenv import load_dotenv
 
+load_dotenv('.env')
 
-def __load_env_variables(func):
-    @wraps(func)  
-    def wrapper(*args, **kwargs):
-        load_dotenv()
-        return func(*args, **kwargs)
-    return wrapper
+with open('./config/settings.toml', 'r') as config_toml:
+    toml_config = toml.load(config_toml)
+
+class UserCredentialsConfig(BaseModel):
+    """
+    Represents user credentials with email and password.
+    Validates the email format using Pydantic's EmailStr and defines the structure
+    for user credentials.
+    """
+    email: EmailStr
+    password: str
+    
+    class Config:
+        toml_prefix = 'login'
+        env_file = '.env'
+        extra = "ignore"
+
+class FloofyPagesConfig(BaseSettings):
+    base: str
+    intake_ny: str
+    intake_la: str
+    intake_fl: str
+
+    class Config:
+        env_prefix = 'FLOOFY_'
+        env_file = '.env'
+        extra = "ignore"
+
+class MetaPagesConfig(BaseSettings):
+    base: str
+    allpharm: str
+    realtime: str
+    vet_orders: str
+
+    class Config:
+        env_prefix = 'META_'
+        env_file = '.env'
+        extra = "ignore"
+
+class SlackConfig(BaseSettings):
+    channel_id: str
+    token: str
+
+    class Config:
+        env_prefix = 'SLACK_'
+        env_file = '.env'
+        extra = "ignore"
 
 
-@__load_env_variables
-def login_config():
-    service_id = os.getenv('SERVICE_ID')
-    account_id = os.getenv('EMAIL')
+EMAIL = os.getenv('EMAIL')
 
-    with open('./config/settings.toml', 'r') as config_toml:
-        user_config = toml.load(config_toml)['login']
-        user_config['account_id'] = account_id
-        user_config['account_key'] = kr.get_password(service_name=service_id, username=account_id)
-    return user_config
+login_config = UserCredentialsConfig(email=EMAIL, 
+                                     password= kr.get_password(service_name=os.getenv('SERVICE_ID'),username=EMAIL))
+ 
+meta_config = MetaPagesConfig()
+floofy_config = FloofyPagesConfig()
+slack_config = SlackConfig()
 
 
-@__load_env_variables
-def floofy_config():
-    with open('./config/settings.toml', 'r') as config_toml:
-        nav_config = toml.load(config_toml)['floofy_pages']
-        nav_config['base'] = os.getenv('FLOOFY', nav_config['base'])
-        nav_config['intake'] = os.getenv("INTAKE", nav_config['intake'])
-    return nav_config
-
-
-@__load_env_variables
-def meta_config():
-    with open('./config/settings.toml', 'r') as config_toml:
-        nav_config = toml.load(config_toml)['meta_pages']
-        nav_config['base'] = os.getenv('META', nav_config['base'])
-        nav_config['allpharm'] = os.getenv('ALLPHARM', nav_config['allpharm'])
-        nav_config['realtime'] = os.getenv('REALTIME', nav_config['realtime'])
-        nav_config['vet_orders'] = os.getenv('VET_ORDERS', nav_config['vet_orders'])
-    return nav_config
-
-
-@__load_env_variables
-def slack_config():
-    with open('./config/settings.toml', 'r') as config_toml:
-        coms_config = toml.load(config_toml)['slack']
-        coms_config['channel_id'] = os.getenv('CHANNEL_ID', coms_config['channel_id'])
-        coms_config['token'] = os.getenv('SLACK_TOKEN', coms_config['token'])
-    return coms_config
 
 
 if __name__ == '__main__':
--- site_automation/site_nav.py
+++ site_automation/site_nav.py
@@ -1,171 +1,143 @@
-from importlib.metadata import files
+import site
 import sys
 import os
-from time import sleep
+import asyncio
+import re
+import time
+from pprint import pprint
+from datetime import datetime
+from typing import Literal
+from pydantic import BaseModel
+from playwright.async_api import async_playwright, expect, TimeoutError
 
 parent_dir = os.path.dirname(os.path.dirname(__file__))
 
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
 
-
-import asyncio
-import glob
-import re
-from datetime import datetime
-from typing import Literal, TypedDict
-from pydantic import BaseModel, EmailStr, ValidationError
-from playwright.async_api import async_playwright, expect
 from config import settings
 
 
-class Login(BaseModel):
-    """
-    Represents user login credentials with email and password.
-
-    Validates the email format using Pydantic's EmailStr.
-
-    Attributes:
-        email (EmailStr): Valid email address.
-        password (str): User's password.
-    """
-    email: EmailStr
-    password: str
-
-class Credentials(TypedDict):
-    """
-    This class is used to define the structure of a dictionary holding user credentials,
-    specifically containing 'email' and 'password' fields.
-
-    Attributes:
-        email (str): The email address of the user.
-        password (str): The password for the user's account.
-        """
-    email: str
-    password: str
-
-LOGIN = Login(email= settings.login_config()['account_id'],
-              password=settings.login_config()['account_key'])
-
-
-FLOOFY = settings.floofy_config()
-
-
-Labs = Literal['NY', 'LA', 'FL']
+LOGIN = settings.login_config
+FLOOFY = settings.floofy_config
+META = settings.meta_config
 
 class LabSelection(BaseModel):
-    lab : Labs
+    """Represents the selection of a lab with a specified location."""
+    lab : Literal['NY', 'LA', 'FL']
 
 
 
 class SiteAutomation:
-    def __init__(self, headless: bool, credentials: Credentials, login_url:str, lab:LabSelection) -> None:
+    """
+    Automates site interaction tasks such as login, navigation, and screenshot capture.
+    Designed to work with headless browsers using the Playwright library.
+    """
+    def __init__(self, headless: bool, credentials: settings.UserCredentialsConfig, login_url:str) -> None:
         self.headless = headless
         self.login_url = login_url
-        self.site = lab if lab else 'meta'
         self.browser = None
         self.context = None
         self.page = None
+        self.__default_path = os.path.join(os.environ['USERPROFILE'], 'Downloads') if os.name == 'nt' else os.path.join(
+        os.path.expanduser('~'), 'Downloads')
+        self.__default_folder = None
         self.credentials = credentials
-        self.__storage_name = ''
 
 
-    async def main(self):
+    async def floofy(self, navigate_to:str, lab:LabSelection,):
         async with async_playwright() as p:
             self.browser = await p.chromium.launch(headless=self.headless)
-            if os.path.exists(self.__storage_name):
-                self.context = await self.browser.new_context(storage_state=self.__storage_name)
-                self. page = await self.context.new_page()
-                await self.navigate(FLOOFY['intake'])
-                await self.page.locator('//*[@id="root"]/div/div[2]/div/div/div[2]/div[1]/div/div[2]/div/div/div[2]/label/span/span').click()
-                await self.screenshot(file_path='test.png')
-                await self.close_browser()
+            if os.path.exists('../automation/mixlabstate.json'):
+                    self.context = await self.browser.new_context(storage_state='mixlabstate.json')
+                    await self.context.tracing.start(screenshots=True, snapshots=True)
+                    self.page = await self.context.new_page()
             else:
                 self.context = await self.browser.new_context()
-
-                self. page = await self.context.new_page()
-
-                await self.login(username=self.credentials.email,password=self.credentials.password)
-                await self.__validate_login() # find a more efficient way later
-                self.__storage_name = self.__generate_storage_filename(identifier=await self.page.title())
-                await self.__save_storage_state()
-
-
-                await self.navigate(FLOOFY['intake'])
-                
-                await self.page.locator('//*[@id="root"]/div/div[2]/div/div/div[2]/div[1]/div/div[2]/div/div/div[2]/label/span/span').click()
-                text = await self.page.get_by_role('value', name='1').text_content()
-                print(text)
-                
-                await self.screenshot(file_path='test.png')
+                self.page = await self.context.new_page()
+                await self.login(login_url=self.login_url, username=self.credentials.email, password=self.credentials.password)
+                await expect(self.page.locator('id=LOGGED_IN_BAR')).to_be_visible()
+                await self.context.storage_state(path=f'{(await self.page.title()).lower()}state.json')
+            try:
+                await self.navigate(navigate_to)
+                await asyncio.sleep(5)
+                screenshot_path = os.path.join(self.__default_path, f'{lab}floofy.png')
+                await self.page.screenshot(full_page=True, path=screenshot_path)
+            except TimeoutError as e:
+                asyncio.sleep(5)
+                await self.context.tracing.stop(path='trace.zip')
+            finally:
                 await self.close_browser()
 
-    
 
-    async def __save_storage_state(self):
-        if self.__storage_name:
-            await self.context.storage_state(path=self.__storage_name)
+    async def meta(self, navigate_to:str):
+        async with async_playwright() as p:
+            self.browser = await p.chromium.launch(headless=self.headless)
+            self.context = await self.browser.new_context()
+            self.page = await self.context.new_page()
+
+            await self.login(login_url=self.login_url, username=self.credentials.email, password=self.credentials.password )
+            await expect(self.page).to_have_title('Login · Metabase')
+            await self.navigate(navigate_to, sleep=2)
+            await self.screenshot(f'meta.png')
+            await self.close_browser()
 
 
-    async def navigate(self, page ):
+    async def __is_logged_in(self):
+        """Checks if the user is logged in based on session cookies."""
+        cookies = await self.page.context.cookies()
+        # Look for a specific cookie known to be set after login, e.g., a session cookie
+        for cookie in cookies:
+            if cookie['name'] == 'session_id':  # Example cookie name
+                return True
+        return False
+
+
+    async def navigate(self, page:str , sleep:int|None=None):
+        """Navigates to a specified page, optionally waiting for a given time."""
+        if sleep:
+            await asyncio.sleep(sleep)
         await self.page.goto(page)
 
 
-    
-
-    
-    async def login(self, username:str, password: str):# loaded:str|None = None):
-        
-        await self.page.goto(url=self.login_url)
-
+    async def login(self, username:str, password: str, login_url:str):# loaded:str|None = None):
+        """Performs login action on the site."""
+        await self.page.goto(url=login_url)
         await self.page.get_by_label(re.compile('(email|username)', re.IGNORECASE)).fill(username)
         await self.page.get_by_label('password').fill(password)
-
         await expect(self.page.get_by_label(re.compile('(email|username)', re.IGNORECASE))).to_have_value(username)
         await expect(self.page.get_by_label('password')).to_have_value(password)
-
-        await self.page.get_by_role('button', name=re.compile('(submit|log in)', re.IGNORECASE)).click()
+        await self.page.get_by_role('button', name=re.compile('(sign in|log in)', re.IGNORECASE)).click()
            
-
     
-    async def __validate_login(self): 
-        await expect(self.page.locator('id=LOGGED_IN_BAR')).to_be_visible() 
-
-
-    async def screenshot(self, file_path,):
-        await asyncio.sleep(5)
-        await self.page.screenshot(timeout=50000, full_page=True, path=file_path)
-        
-        
-
-    async def __save_storage_state(self):
-        if self.__storage_name:
-            await self.context.storage_state(path=self.__storage_name)
     
- 
-
-
     async def close_browser(self):
+        """Closes the browser and all associated contexts."""
         if self.context:
             await self.context.close()
         if self.browser:
             await self.browser.close()
 
-    def __generate_storage_filename(self, identifier: str = None) -> str:
-            base_name = identifier if identifier else "storage_state"
-            timestamp = datetime.now().strftime('%Y%m%d')
-            return f"{base_name}_{timestamp}.json"
-    
-    def __del_storage_state(self):
-        if os.path.exists(self.__storage_name):
-            os.remove(self.__storage_name)
+ 
 
+# async def main():
+#         s = time.perf_counter()
+
+#         mixlab_meta = SiteAutomation(headless=False, credentials=LOGIN, login_url= META['base'])
+#         mixlab_floofy = SiteAutomation(headless=False, credentials=LOGIN, login_url= FLOOFY['base'])
+
+#         task = [
+#             mixlab_floofy.floofy(FLOOFY['intake_ny'], lab='NY'),
+#             mixlab_meta.meta(META['allpharm'])
+#         ]
+
+#         await asyncio.gather(*task)
+
+#         elapsed = time.perf_counter() - s
+#         print(f'{__file__} excute in {elapsed:0.2f} seconds')
 
 if __name__ == '__main__':
-    import time
-    s = time.perf_counter()
-    site = SiteAutomation(headless=False, credentials=LOGIN, login_url= FLOOFY['base'], lab='NY')
-    asyncio.run(site.main())
-    elapsed = time.perf_counter() - s
-    print(f'{__file__} excute in {elapsed:0.2f} seconds')
+    site = SiteAutomation(headless=False,credentials=LOGIN, login_url=FLOOFY.base)
+    asyncio.run(site.floofy(FLOOFY.intake_fl, lab='FL'))
+    asyncio.run(site.floofy(FLOOFY.intake_ny, lab='NY'))
     
