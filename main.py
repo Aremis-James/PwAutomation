6ab1c7c78cca2b13087f56f80f6937f012b4a338
AremisJ <devpythonaj@gmail.com>
2024-03-02 11:37:26 -0500

    add schedule & logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import toml
 import asyncio
+import logging
 import os
 import time
 import schedule
@@ -10,6 +11,11 @@ from site_automation.navigation import FloofyAutomation, MetaAutomation
 
 
 load_dotenv()
+
+logging.basicConfig(filename='automation_log.txt', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
 
 PASSWORD = kr.get_password(service_name=os.getenv('SERVICE_ID'),username=EMAIL)
 LOGIN = UserCredentialsConfig(email=EMAIL, password=PASSWORD)
@@ -30,14 +36,29 @@ async def main():
         ]
         try:
             await asyncio.gather(*task)
+            logging.info("All tasks completed successfully.")
         except Exception as e:
-             print(f'{e}')
+            logging.error(f'Exception occurred: {e}', exc_info=True)
 
         elapsed = time.perf_counter() - s
-        print(f'{__file__} excute in {elapsed:0.2f} seconds')
+        logging.info(f'{__file__} excute in {elapsed:0.2f} seconds')
 
  
-
+def run_tasks():
+     asyncio.run(main())
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+    for day in weekdays:
+        for hour in range(8, 20):  # From 8 AM to 7PM
+            schedule.every().__getattribute__(day).at(f'{hour:02d}:00').do(run_tasks)
+
+    # Schedule tasks for Saturday
+    for hour in range(8, 17):  # From 8 AM to 4 PM
+        schedule.every().saturday.at(f"{hour:02d}:00").do(run_tasks)
+    schedule.every().saturday.at("16:30").do(run_tasks)
+
+    logging.info("Scheduler started. Waiting for tasks to run.")
+    while True:
+        schedule.run_pending()
+        time.sleep(1300) # check every hour
