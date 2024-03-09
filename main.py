1774aaef37791b5de4e3f63358fdb4f488df57d0
Aremis-James <129014604+Aremis-James@users.noreply.github.com>
2024-03-09 15:39:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import toml
 import asyncio
 import logging
 import os
 import time
-import schedule
 import keyring as kr
-from dopplersdk import DopplerSDK
 from dotenv import load_dotenv
-from pprint import pprint
 from communication.slackbot import SlackComs
 from config.settings import UserCredentialsConfig,  MetaPagesConfig, FloofyPagesConfig, EMAIL, doppler_config_secrets
 from site_automation.navigation import FloofyAutomation, MetaAutomation
@@ -57,37 +53,9 @@ def run_tasks():
      asyncio.run(main())
 
 if __name__ == '__main__':
-    # sdk = DopplerSDK()  
-    # sdk.set_access_token('dp.st.dev_aremis-james.nFdJjuieucmmqvnkKDARlo0pcNyErsQoZImaBVn8uuO')
-
-    # results = sdk.secrets.get(
-    #      project='mixlab-scrapper',
-    #      config='dev_aremis-james',
-    #      name='EMAIL'
-
-    # )
-
-    # pprint(vars(results)['value']['raw'])
-
-
     slack = SlackComs(token=SLACK_TOKEN, channel=CHANNEL)
     results = asyncio.run(main())
     if results:
         for index in range(len(results)):
             slack.upload(results[index][1])
 
-
-    # weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
-    # for day in weekdays:
-    #     for hour in range(8, 20):  # From 8 AM to 7PM
-    #         schedule.every().__getattribute__(day).at(f'{hour:02d}:00').do(run_tasks)
-
-    # # Schedule tasks for Saturday
-    # for hour in range(8, 17):  # From 8 AM to 4 PM
-    #     schedule.every().saturday.at(f"{hour:02d}:00").do(run_tasks)
-    # schedule.every().saturday.at("16:30").do(run_tasks)
-
-    # logging.info("Scheduler started. Waiting for tasks to run.")
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1300) # check every hour~
